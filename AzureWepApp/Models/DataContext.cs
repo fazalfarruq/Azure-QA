bd66c6f8b7822da0b21c887c3aea290545cdf7f0
fazalfarruq <fazal@lucidia.com>
2019-05-16 13:33:42 -0400

    fixed things

--- AzureWepApp/Models/DataContext.cs
+++ AzureWepApp/Models/DataContext.cs
@@ -6,33 +6,15 @@ namespace AddressWebApp.Models
 {
     public class DataContext : DbContext
     {
-
-        public DataContext()
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
         }
-        public DataContext(DbContextOptions<DataContext> options)
-            : base(options)
-        {
-        }
-
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    var builder = new ConfigurationBuilder()
-        //                        .SetBasePath(Directory.GetCurrentDirectory())
-        //                        .AddJsonFile("appsettings.json");
-        //    var configuration = builder.Build();
-        //    optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
-
-        //}
-
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    base.OnModelCreating(builder);
-        //    // Customize the ASP.NET Identity model and override the defaults if needed.
-        //    // For example, you can rename the ASP.NET Identity table names and more.
-        //    // Add your customizations after calling base.OnModelCreating(builder);
-        //}
 
         public DbSet<Address> Addresses { get; set; }
 
