a66101eb10a24473efc5cd2c168c3a0076d0b61d
fazalfarruq <fazal@lucidia.com>
2019-05-16 13:24:51 -0400

    changed commit

--- AzureWepApp/Models/DataContext.cs
+++ AzureWepApp/Models/DataContext.cs
@@ -6,15 +6,33 @@ namespace AddressWebApp.Models
 {
     public class DataContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var builder = new ConfigurationBuilder()
-                                .SetBasePath(Directory.GetCurrentDirectory())
-                                .AddJsonFile("appsettings.json");
-            var configuration = builder.Build();
-            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
 
+        public DataContext()
+        {
         }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //                        .SetBasePath(Directory.GetCurrentDirectory())
+        //                        .AddJsonFile("appsettings.json");
+        //    var configuration = builder.Build();
+        //    optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //    // Customize the ASP.NET Identity model and override the defaults if needed.
+        //    // For example, you can rename the ASP.NET Identity table names and more.
+        //    // Add your customizations after calling base.OnModelCreating(builder);
+        //}
 
         public DbSet<Address> Addresses { get; set; }
 
--- AzureWepApp/Startup.cs
+++ AzureWepApp/Startup.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AddressWebApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace AddressWebApp
 {
@@ -13,8 +16,18 @@ namespace AddressWebApp
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
