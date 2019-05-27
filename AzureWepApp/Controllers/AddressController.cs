6c109848f0e272fb0287832d9b10520c5958ade7
iamfazalf <fazal.farruq@sobeys.com>
2019-05-27 12:04:14 -0400

    added appsetting config Greetings

--- AzureWepApp/Controllers/AddressController.cs
+++ AzureWepApp/Controllers/AddressController.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace AddressWebApp.Controllers
 {
     public class AddressController : Controller
     {
+        public IConfiguration Configuration { get; }
+
+        public AddressController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }                      
+
         [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            var model = Configuration["Greeting"];
+            return View("Index",model);
         }
     }
 }
